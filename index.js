faf1d50821310cd95edef866b4040445e938723b
Fabien Franzen <info@atelierfabien.be>
2015-12-17 21:23:54 +0100

    Expose Agenda and mubsub through index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+exports.Agenda = require('agenda');
+exports.mubsub = require('mubsub');
+
 exports.Worker = require('./lib/worker');
 exports.Task = require('./lib/task');
 exports.support = require('./lib/support');
